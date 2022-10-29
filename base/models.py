672a46c38a247fd59b333c60127fdd1eac3e7ced
RadimMěkýš <radamekys2@seznam.cz>
2022-10-29 19:06:39 +0200

    Předěláno topic na typ práce

--- base/models.py
+++ base/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Topic(models.Model):
+class Typ(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
@@ -11,7 +11,7 @@ class Topic(models.Model):
 
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    typprace = models.ForeignKey(Typ, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     #participants =
